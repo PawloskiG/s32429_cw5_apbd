90d631064a4fed92cf870bfe43d3cc78e246587d
Paweł Gębicki <s32429@pjwstk.edu.pl>
2026-06-29 16:57:16 +0200

    Web API Project

--- cw5/Program.cs
+++ cw5/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Rejestracja kontrolerów (Web API oparte na kontrolerach).
+builder.Services.AddControllers();
+
+// Swagger / OpenAPI - przydatne do podglądu API obok testów w Postmanie.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// Mapowanie tras do kontrolerów.
+app.MapControllers();
 
 app.Run();
